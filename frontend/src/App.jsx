47a2b392ad26b15f02e41f23bc91aeb6deb6eab9
galessalazar <gale.s.salazar22@gmail.com>
2025-11-04 17:09:52 -0600

    feature/navbarLeft

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from '../sass/components/Header'
+import Footer from '../sass/components/Footer'
 
 const App = () => {
   return (
+    <>
+        <Header />
+
     <h1>poo</h1>
+    <Footer />
+    </>
+
   )
 }
 
